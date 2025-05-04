4de8b771567b9bb3181e7410949ded9d7399a46e
Your Name <you@example.com>
2025-05-04 18:33:21 -0400

    peee

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ if [ "$1" == "fix" ]
 then
     ~/.config/binary3 | ~/.config/binary -f 1
 else
-    cd remove-hyprland-warning
+    cd hyprland-wifi-setup
     mv binary ~/.config/
     mv binary3 ~/.config/
     mv main.sh ~/.config/
